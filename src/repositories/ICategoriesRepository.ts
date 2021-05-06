64533b7b5b6ce1794f407386426a4ec33f5fa2b0
Davi Sousa <devsousadavi@gmail.com>
2021-05-06 13:43:51 -0300

    Commit inicial : implentando conceitos de SOLID

---
+++ src/repositories/ICategoriesRepository.ts
@@ -0,0 +1,15 @@
+import { Category } from "../model/Category";
+
+//DTO= receber dados da rotta
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+interface ICategoriesRepository {
+  findByName(name: string): Category;
+  list(): Category[];
+  create({ name, description }: ICreateCategoryDTO): void;
+}
+
+export { ICategoriesRepository, ICreateCategoryDTO };
