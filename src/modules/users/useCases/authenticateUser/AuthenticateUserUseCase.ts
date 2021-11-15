590fb7d5e08c1b04ee4753c4c1435438601c9e31
devDaviSousa <devsousadavi@gmail.com>
2021-11-15 14:44:28 -0300

    corrigindo o bcrypt

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,64 +1,64 @@
-import {compare} from "bcrypt"
+import { compare } from "bcryptjs"
 import { inject, injectable } from "tsyringe";
-import {sign} from "jsonwebtoken"
+import { sign } from "jsonwebtoken"
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { AppError } from "../../../../erros/AppError";
 
-interface IResquest{
-    email:string;
-    password:string;
+interface IResquest {
+    email: string;
+    password: string;
 }
 
-interface IResponse{
-    user:{
-        name:string;
-        email:string;
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
     };
-    token:string;
+    token: string;
 }
 
 
 @injectable()
-class AuthenticateUserUseCase{
-    
-    constructor(
-    @inject("UsersRepository")
-        private usersRepositoy:IUsersRepository
-    ){}
+class AuthenticateUserUseCase {
 
-    async execute({email,password}:IResquest):Promise<IResponse>{
+    constructor(
+        @inject("UsersRepository")
+        private usersRepositoy: IUsersRepository
+    ) { }
+
+    async execute({ email, password }: IResquest): Promise<IResponse> {
 
         const user = await this.usersRepositoy.findByEmail(email);
 
-        if(!user){
-            throw new AppError ("Email or password incorrect");
+        if (!user) {
+            throw new AppError("Email or password incorrect");
         };
 
-        const passworMath = await compare(password,user.password);
+        const passworMath = await compare(password, user.password);
 
-        if(!passworMath){
-            throw new AppError ("Email or password incorrect");
+        if (!passworMath) {
+            throw new AppError("Email or password incorrect");
         };
 
         //criar tokern
 
-        const token = sign({},"f4cc08fa20e1167cba660c5fae1f2a1a",{
-            subject:user.id,
-            expiresIn:"1d"
+        const token = sign({}, "f4cc08fa20e1167cba660c5fae1f2a1a", {
+            subject: user.id,
+            expiresIn: "1d"
         });
 
-        const tokenReturn:IResponse = {
+        const tokenReturn: IResponse = {
             token,
-            user:{
-                name:user.name,
-                email:user.email
+            user: {
+                name: user.name,
+                email: user.email
             }
         }
 
-        return tokenReturn         
+        return tokenReturn
 
     }
 
 }
 
-export {AuthenticateUserUseCase}
+export { AuthenticateUserUseCase }
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from "tsyringe";
-import {hash} from "bcrypt";
+import { hash } from "bcryptjs";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { AppError } from "../../../../erros/AppError";
@@ -14,15 +14,15 @@ class CreateUserUseCase {
 
   async execute({ name, email, password, drive_license }: ICreateUserDTO): Promise<void> {
 
-    const passwordHash = await hash(password,8);
+    const passwordHash = await hash(password, 8);
 
     const userAlreadyExists = await this.userRepository.findByEmail(email);
 
-    if(userAlreadyExists){
+    if (userAlreadyExists) {
       throw new AppError("User Already Exixts");
     }
 
-    await this.userRepository.create({ name, email, password:passwordHash, drive_license });
+    await this.userRepository.create({ name, email, password: passwordHash, drive_license });
 
   }
 
